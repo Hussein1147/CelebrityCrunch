ad842e6d4acd7e6b82f101af3dbf72e52c9d55d7
Djibril Keita <djibril@TheWorkplacestartup.com>
2014-10-30 17:55:19 -0400

    Initial Commit

---
+++ CelebrityCrunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CelebrityCrunch
+//
+//  Created by DJIBRIL KEITA on 10/30/14.
+//  Copyright (c) 2014 DJIBRILKEITA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CelebrityCrunch/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  CelebrityCrunch
+//
+
+//  Copyright (c) 2014 DJIBRILKEITA. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
