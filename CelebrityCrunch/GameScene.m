c810ba53588212f61573ee05542ecee4c8a7406f
Djibril Keita <djibril@TheWorkplacestartup.com>
2015-11-02 18:55:19 -0500

    First commit

--- CelebrityCrunch/GameScene.m
+++ CelebrityCrunch/GameScene.m
@@ -27,15 +27,15 @@ static const CGFloat TileHeight = 95.0;
     if ((self = [super initWithSize:size])) {
         
         self.anchorPoint = CGPointMake(0.5, 0.5);
-        
-        SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"Background-1"];
+
+        SKSpriteNode *background = [[SKSpriteNode alloc] initWithColor:[self colorWithHexString:@"FF6666"] size:[UIScreen mainScreen].bounds.size];
         [self addChild:background];
 
         self.gameLayer = [SKNode node];
         self.gameLayer.hidden = YES;
 
         [self addChild:self.gameLayer];
-        CGPoint layerPosition = CGPointMake(-TileWidth * NumColumns/2, -TileHeight*NumRows + self.size.height/5 + (TileWidth/10));
+        CGPoint layerPosition = CGPointMake(-TileWidth * NumColumns/2, -TileHeight*NumRows + self.size.height/4 + (TileWidth/10));
         self.actorLayer= [SKNode node];
         self.actorLayer.position = layerPosition;
         self.tilesLayer = [SKNode node];
@@ -47,6 +47,41 @@ static const CGFloat TileHeight = 95.0;
         
     }
     return self;
+}
+-(UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+
+    if ([cString length] != 6) return  [UIColor grayColor];
+
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
 }
 #pragma mark - show/hide actor selection
 -(void)showSelectionIndicatorsForactor:(RWTActor*)actor{
