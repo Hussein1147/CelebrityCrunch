661341d92bd1327d5eafb07d7e605ab1473dab6e
Djibril <Djibrilhms@gmail.com>
2018-05-24 14:36:59 -0400

    attempted to correct
    missing files

--- Podfile
+++
@@ -1,5 +0,0 @@
-platform :ios,'8.0'
-pod 'Canvas'
-target :CelebrityCrunchTests, :exclusive => true do
-  pod 'OCMockito', '~> 1.0'
-end
--- Pods/Target Support Files/Pods-CelebrityCrunchTests/Pods-CelebrityCrunchTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CelebrityCrunchTests : NSObject
-@end
-@implementation PodsDummy_Pods_CelebrityCrunchTests
-@end
